52ef14567fd58e689fa9a3b1b1c91b0850c76828
magali09 <magalicristofori@hotmail.com>
2023-05-28 21:43:48 -0300

    integrador

--- integrador02.py
+++ integrador02.py
@@ -1,5 +1,40 @@
+# 0. Crear la función 'stark_normalizar_datos' la cual recibirá por parámetro la lista
+# de héroes. La función deberá:
+# ● Recorrer la lista y convertir al tipo de dato correcto las keys (solo con
+# las keys que representan datos numéricos)
+# ● Validar primero que el tipo de dato no sea del tipo al cual será
+# casteado. Por ejemplo si una key debería ser entero (ejemplo edad)
+# verificar antes que no se encuentre ya en ese tipo de dato.
+# ● Si al menos un dato fue modificado, la función deberá imprimir como
+# mensaje ‘Datos normalizados’, caso contrario no imprimirá nada.
+# ● Validar que la lista de héroes no esté vacía para realizar sus acciones,
+# caso contrario imprimirá el mensaje: “Error: Lista de héroes vacía”
+
+# 1.1. Crear la función 'obtener_nombre' la cual recibirá por parámetro un
+# diccionario el cual representara a un héroe y devolverá un string el cual
+# contenga su nombre formateado de la siguiente manera:
+# Nombre: Howard the Duck
+
+# 1.2. Crear la función 'imprimir_dato' la cual recibirá por parámetro un string y
+# deberá imprimirlo en la consola. La función no tendrá retorno.
+
+# 1.3. Crear la función 'stark_imprimir_nombres_heroes' la cual recibirá por
+# parámetro la lista de héroes y deberá imprimirla en la consola. Reutilizar las
+# funciones hechas en los puntos 1.1 y 1.2. Validar que la lista no esté vacía
 from data_stark import lista_personajes
 
-def stark_normalizar_datos(lista_personajes)->list:
+
+def stark_normalizar_datos(lista_personajes)->list:#lista
+    # '''convierto los datos de string a numerico'''
+    
     for data in lista_personajes:
-        if data == int:
+        if data["altura"]:
+            altura = float(data["altura"])
+            
+            peso = float(data["peso"])
+            fuerza = int(data["fuerza"])
+    print(f"{altura},{peso},{fuerza}")    
+stark_normalizar_datos() 
+
+# print(lista_personajes)
+
